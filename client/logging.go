ec7546aae92b931183e6443429df25b9880f0f43
Mike Frisch <mikef17@gmail.com>
2019-04-17 09:08:43 -0400

    Implement common logging interface

---
+++ client/logging.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the LICENSE.md file
+// distributed with the sources of this project regarding your rights to use or distribute this
+// software.
+
+package client
+
+// Logger provides a consistent interface for logging in scs-library-client
+type Logger interface {
+	Info(...interface{})
+	Infof(string, ...interface{})
+	Debug(...interface{})
+	Debugf(string, ...interface{})
+}
